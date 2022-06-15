14c4ba5a34745f1b66bab4791139ebc82d5bd53f
DESKTOP-PFQQR2S\quwer <quwert86@gmail.com>
2022-06-15 15:48:10 +0300

    2 semenar added

--- Program.cs
+++ Program.cs
@@ -135,10 +135,98 @@
 // Напишите программу, которая на вход принимает число (N),
 //  а на выходе показывает все чётные числа от 1 до N.
 
-Console.WriteLine("Type int N");
-int n = Convert.ToInt32(Console.ReadLine());
-int current = 2;
-while (current <= n){
-    Console.Write(current + " ");
-    current = current + 2;
+// Console.WriteLine("Type int N");
+// int n = Convert.ToInt32(Console.ReadLine());
+// int current = 2;
+// while (current <= n){
+//     Console.Write(current + " ");
+//     current = current + 2;
+// }
+
+//  2 Seminar
+
+
+// void ShowNumber(){
+//     int num = new Random().Next(10, 100);
+//     int firstNum = num /10;
+//     int secondNum = num % 10;
+//     Console.Write(num + "->");
+//     if (firstNum > secondNum){
+//         Console.WriteLine(firstNum);
+//     }
+//     else {
+//         Console.WriteLine(secondNum);
+//     }
+// }
+
+// ShowNumber();
+
+
+
+// Напишите программу, которая выводит случайное трехзначное число и удаляет вторую цифру этого числа.
+
+
+
+// int NumberWithOutSecDig(int Number){   
+//     int NewNumber = Number / 100 * 10 + Number % 10;
+//     return NewNumber;
+// }
+// int Number = new Random().Next(100, 1000);
+// int res = NumberWithOutSecDig(Number);
+// Console.WriteLine(Number + "  " + res);
+
+
+// Напишите программу, которая будет принимать на вход два числа и выводить, является ли второе число кратным первому. Если число 2 не кратно числу 1, то программа выводит остаток от деления.
+
+
+
+// void resultOfDivision(int a, int b){
+//     if (b % a==0){
+//         Console.WriteLine(b + " кратно "  + a );
+//     }
+//     else{
+//     Console.WriteLine(b + " не кратно " + a + " остаток " + b%a);
+//     }
+// }
+// Console.WriteLine("Type int N1, N2");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+// int n2 = Convert.ToInt32(Console.ReadLine());
+
+// resultOfDivision(n1,n2);
+
+
+
+// Напишите программу, которая принимает на вход число и проверяет, кратно ли оно одновременно 7 и 23.
+
+// void resultOfDivision(int a){
+//     if (a % 7 == 0 && a % 23 == 0 ){
+//         Console.WriteLine(a + " кратно " );
+//     }
+//     else{
+//     Console.WriteLine(a + " не кратно ");
+//     }
+// }
+// Console.WriteLine("Type int N1");
+// int n1 = Convert.ToInt32(Console.ReadLine());
+
+
+// resultOfDivision(n1);
+
+
+// Напишите программу, которая принимает на вход два числа и проверяет, является ли одно число квадратом другого.
+
+void resultOfDivision(int a, int b){
+    if (a*a==b){
+        Console.WriteLine(b + " квадрат "  + a );
+    }
+    else{
+    Console.WriteLine(b + " не квадрат " + a );
+    }
 }
+
+
+Console.WriteLine("Type int N1, N2");
+int n1 = Convert.ToInt32(Console.ReadLine());
+int n2 = Convert.ToInt32(Console.ReadLine());
+
+resultOfDivision(n1,n2);
